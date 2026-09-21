d3b475ffb4c55145fc36ce47569404d1dbad1b7d
alvargu <92863607+alvargu@users.noreply.github.com>
2026-09-21 14:54:27 +0200

    Small changes to part one assignment 1 - representing the entire audio signal. Audio signals added to repo

--- Assignments/assignment1/audio_signal.py
+++ Assignments/assignment1/audio_signal.py
@@ -14,18 +14,17 @@ audioFilePath = "MusicBoxNotes_fixed.wav"
 fs, audioRecording = wav.read(audioFilePath)
 # My audio file is stereo, so I will take only one channel for analysis
 monoRecording = audioRecording[:, 0]
+wav.write("MusicBoxNotesMono.wav", fs, monoRecording)
 # Print out some data about the audio file
 t = np.linspace(0, len(monoRecording)/fs, len(monoRecording))
 
 # Define the figure for plotting
 audioPlot = plt.figure(figsize=(12, 6))
-
 # Plot the original audio signal
 plt.plot(t, monoRecording)
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
 plt.title("Audio Signal")
-
 # Save and display the plot
 plt.savefig("audio_signal.png")
 plt.show()
